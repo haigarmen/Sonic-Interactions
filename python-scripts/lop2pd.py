0085c43dc873c7403138237c53dff982afa953ba
Haig Armen <haig@liftstudios.ca>
2018-10-11 16:12:27 -0500

    an update from the Chicago Workshop

--- python-scripts/lop2pd.py
+++ python-scripts/lop2pd.py
@@ -87,6 +87,6 @@ while True:
         send2Pd(message)
 
 # consider creating a message that has all values in one string rather than separate messages
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+#    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
 #    print(message)
     time.sleep(waitTime)
