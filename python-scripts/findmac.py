3b9f0ec9674de39a3e4d09b6d61c265b86cb3a29
Haig Armen <haig.armen@gmail.com>
2019-03-07 15:44:06 -0600

    added sending messages to Pd to python script

--- python-scripts/findmac.py
+++ python-scripts/findmac.py
@@ -1,8 +1,21 @@
-# simple inquiry example
+#!/usr/bin/python3
+
+import os
+import time
 import bluetooth
 
-nearby_devices = bluetooth.discover_devices(lookup_names=True)
-print("found %d devices" % len(nearby_devices))
+# my python script that communications with Pd
 
-for addr, name in nearby_devices:
-    print("  %s - %s" % (addr, name))
+def send2Pd(message=''):
+    # Send a message to Pd
+    os.system("echo '" + message + "' | pdsend 3000 localhost udp")
+
+while True:
+    nearby_devices = bluetooth.discover_devices(lookup_names=True)
+    print("found %d devices" % len(nearby_devices))
+
+    for addr, name in nearby_devices:
+        print("  %s - %s" % (addr, name))
+        message = '0 ' + addr # make a string for use with Pdsend
+        send2Pd(message)
+    time.sleep(10)
